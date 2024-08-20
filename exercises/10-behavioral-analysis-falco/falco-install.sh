c74dd7726466d94fd7f484f65bc9aa5f55c48cc6
Antonino <a.bonanno@gmail.com>
2024-08-20 19:49:57 +0200

    Update falco-install.sh - adding sudo where it fits

--- exercises/10-behavioral-analysis-falco/falco-install.sh
+++ exercises/10-behavioral-analysis-falco/falco-install.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-curl -s https://falco.org/repo/falcosecurity-packages.asc | apt-key add -
-echo "deb https://download.falco.org/packages/deb stable main" | tee -a /etc/apt/sources.list.d/falcosecurity.list
-apt-get update -y
-apt install -y dkms make linux-headers-$(uname -r)
-apt install -y clang llvm
-apt install -y dialog
+curl -s https://falco.org/repo/falcosecurity-packages.asc | sudo apt-key add -
+echo "deb https://download.falco.org/packages/deb stable main" | sudo tee -a /etc/apt/sources.list.d/falcosecurity.list
+sudo apt-get update -y
+sudo apt install -y dkms make linux-headers-$(uname -r)
+sudo apt install -y clang llvm
+sudo apt install -y dialog
 sudo apt-get install -y falco=0.33.1
